8494f8222fef75ca19afc77a7538adf1f582ed40
RAVI <ravi13eng@gmail.com>
2021-02-11 20:12:51 +0530

    Updates

---
+++ precache-manifest.aefb28a0a66bf6dc2f5128eec6636b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129836fb5621345abda407bb7b4501f8",
+    "url": "/movie-react-app/index.html"
+  },
+  {
+    "revision": "7381bcec22684d04d776",
+    "url": "/movie-react-app/static/css/main.a0c523af.chunk.css"
+  },
+  {
+    "revision": "51a2938f24eeb97bab83",
+    "url": "/movie-react-app/static/js/2.99089c4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-react-app/static/js/2.99089c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7381bcec22684d04d776",
+    "url": "/movie-react-app/static/js/main.dbf1fd99.chunk.js"
+  },
+  {
+    "revision": "35d55cc8a34b6b3d7c4d",
+    "url": "/movie-react-app/static/js/runtime-main.a95e4e81.js"
+  }
+]);
